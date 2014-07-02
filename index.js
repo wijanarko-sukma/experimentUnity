7de7fa814f8a93239a52f8d92d2aef0b6fd191ae
Wijanarko Sukma <wijanarko.sukma@gmail.com>
2014-07-02 10:29:15 +0700

    test initial nodejs

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require('http'),
+    express = require('express'),
+    path = require('path');
+ 
+//2 
+var app = express();
+app.set('port', process.env.PORT || 3000); 
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.get('/', function (req, res) {
+  res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
